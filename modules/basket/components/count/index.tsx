baa741f10ca947b70eca18fc66565274c7c2691d
Your Name <serhiivelytchenko@gmail.com>
2021-05-17 16:18:29 +0300

    Initial commit

---
+++ modules/basket/components/count/index.tsx
@@ -0,0 +1,29 @@
+import { Wrapper, ViewButton, CardItem } from './views';
+import { BasketBLContext } from '@md-modules/basket/layers/business';
+import React, { useCallback } from 'react';
+import { ApplianceBasketItem } from '../basket-item';
+
+const Count: React.FC< ApplianceBasketItem > = ({ count, appliance }) => {
+  const { incrementItemCount, decrementItemCount } = React.useContext(BasketBLContext);
+
+  const handleIncrementItemCount = useCallback(() => {
+    incrementItemCount(appliance);
+  }, [appliance, incrementItemCount]);
+
+  const handleDecrementItemCount = useCallback(() => {
+    decrementItemCount(appliance);
+  }, [appliance, decrementItemCount]);
+  return (
+    <Wrapper>
+            <ViewButton onClick={handleDecrementItemCount}>
+                -
+            </ViewButton>
+            <CardItem>{count}</CardItem>
+            <ViewButton onClick={handleIncrementItemCount}>
+                +
+            </ViewButton>
+    </Wrapper>
+  );
+};
+
+export { Count };
---
+++ modules/shared/hooks/useLocalStorage/index.ts
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export default function useLocalStorage<T = any> (key: string, defaultValue: T) {
+  const [value, setValue] = React.useState<T>(defaultValue);
+
+  React.useEffect(() => {
+    const itemsValue = window.localStorage.getItem(key);
+
+    if (itemsValue !== null) {
+      setValue(JSON.parse(itemsValue));
+    }
+  }, [key]);
+
+  React.useEffect(() => {
+    window.localStorage.setItem(key, JSON.stringify(value));
+  }, [key, value]);
+
+  return [value, setValue] as const;
+}
